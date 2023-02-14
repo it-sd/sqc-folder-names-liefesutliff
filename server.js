e7448022543bdab56e004cd06c6b2186504a9ebf
Liefe Sutliff <lsutliff@student.cvtc.edu>
2023-02-14 11:42:01 -0600

    Implemented folder function

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const newFolderName = function (folders) {
-  if(folders.length >= 1) {
-    console.log(`New Folder (${folders.length+1})`);
+  if (folders.length >= 1) {
+    console.log(`New Folder (${folders.length + 1})`)
   } else {
-    console.log('New Folder');
+    console.log('New Folder')
   }
 }
 
 // Test with no folders
 newFolderName([])
 // Test with one folder
-newFolderName(["New Folder"])
+newFolderName(['New Folder'])
 // Test with three folders
-newFolderName(["New Folder", "New Folder (2)", "New Folder (3)"])
+newFolderName(['New Folder', 'New Folder (2)', 'New Folder (3)'])
