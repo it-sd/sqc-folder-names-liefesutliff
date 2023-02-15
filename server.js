684c3b943e569f7d3ee1699c204c8dd55fe6a74f
Liefe Sutliff <lsutliff@student.cvtc.edu>
2023-02-15 08:50:01 -0600

    Updated files

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const newFolderName = function (folders) {
-  if (folders.length >= 1) {
-    console.log(`New Folder (${folders.length + 1})`)
+  const test = Array.from(folders)
+  let n = 1
+
+  if (!folders.includes('New Folder')) {
+    folders.push('New Folder')
+    return folders[folders.length - 1]
+  }
+
+  if (folders.length === 0) {
+    // console.log(`New Folder`)
+    folders.push('New Folder')
+    return folders[0]
   } else {
-    console.log('New Folder')
+    for (let i = 0; i < folders.length; i++) {
+      if (folders.length === 0) {
+        test.push('New Folder')
+      }
+
+      n++
+
+      if (!folders.includes(`New Folder (${n})`)) {
+        test.push(`New Folder (${n})`)
+        return `New Folder (${n})`
+      }
+
+      if (n === folders.length) {
+        test.push(`New Folder (${n + 1})`)
+      } else if (folders.length === 1) {
+        test.push(`New Folder (${n})`)
+      }
+    }
+
+    return test[test.length - 1]
   }
 }
 
-// Test with no folders
-newFolderName([])
-// Test with one folder
-newFolderName(['New Folder'])
-// Test with three folders
-newFolderName(['New Folder', 'New Folder (2)', 'New Folder (3)'])
+module.exports = { newFolderName }
