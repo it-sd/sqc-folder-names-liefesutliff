be532174ef45c8a66fc8ad4ad49f25cde8e3365a
Liefe Sutliff <lsutliff@student.cvtc.edu>
2023-02-17 20:29:33 -0600

    Fixed coverage issues

--- server.js
+++ server.js
@@ -7,31 +7,17 @@ const newFolderName = function (folders) {
     return folders[folders.length - 1]
   }
 
-  if (folders.length === 0) {
-    // console.log(`New Folder`)
-    folders.push('New Folder')
-    return folders[0]
-  } else {
-    for (let i = 0; i < folders.length; i++) {
-      if (folders.length === 0) {
-        test.push('New Folder')
-      }
+  for (let i = 0; i < folders.length; i++) {
+    n++
 
-      n++
-
-      if (!folders.includes(`New Folder (${n})`)) {
-        test.push(`New Folder (${n})`)
-        return `New Folder (${n})`
-      }
-
-      if (n === folders.length) {
-        test.push(`New Folder (${n + 1})`)
-      } else if (folders.length === 1) {
-        test.push(`New Folder (${n})`)
-      }
+    if (!folders.includes(`New Folder (${n})`)) {
+      test.push(`New Folder (${n})`)
+      return `New Folder (${n})`
     }
 
-    return test[test.length - 1]
+    if (n === folders.length) {
+      test.push(`New Folder (${n + 1})`)
+    }
   }
 }
 
